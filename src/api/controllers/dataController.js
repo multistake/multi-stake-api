2c2a6516b675b15055db07ae000097b1a7dc7734
Mahdi Mostafavi <mmostafavi247@gamil.com>
2022-02-28 20:14:19 +0200

    Add Controller functions

--- src/api/controllers/dataController.js
+++ src/api/controllers/dataController.js
@@ -1,6 +1,19 @@
+import ValidatorsDAO from "../../dao/validatorsDAO";
+
 export default class DataController {
-	static async apiGetSearchFormData(req, res, next) {
+	static async apiGetSearchFormData(req, res) {
 		try {
+			const { network } = req.body;
+
+			let { names, asns, softwareVersions, dataCenters } =
+				await ValidatorsDAO.getSearchFormData(network);
+
+			res.send({
+				names,
+				asns,
+				softwareVersions,
+				dataCenters,
+			});
 		} catch (err) {
 			console.error(`Unable to get search-form data in DataController: ${err}`);
 		}
--- src/api/controllers/searchController.js
+++ src/api/controllers/searchController.js
@@ -1,8 +1,63 @@
+import ValidatorsDAO from "../../dao/validatorsDAO";
+
 export default class SearchController {
 	static async apiGetValidators() {}
 
-	static async apiSearchValidators(req, res, next) {
+	static async apiSearchValidators(req, res) {
 		try {
+			const {
+				query: {
+					count,
+					names,
+					dataCenters,
+					dataCenterConcentrationScore,
+					validatorAsn,
+					softwareVersion,
+					validatorActiveStake,
+					receivedStakeFromStakePools,
+					currentValidatorCommission,
+					validatorApy,
+					validatorSkipRate,
+					validatorVotingPerformance,
+				},
+				network,
+			} = req.body;
+
+			// structure of query parameters
+			// {
+			// 	count: Number,
+			// 	names: String[],
+			// 	dataCenters: String[],
+			// 	dataCenterConcentrationScore: Number[],
+			// 	validatorAsn: Number[],
+			// 	softwareVersion: String[],
+			// 	validatorActiveStake: Number[],
+			// 	receivedStakeFromStakePools: Boolean,
+			// 	historical_validator_commission: Number[],
+			// 	currentValidatorCommission: Number[],
+			// 	validatorApy: Number[],
+			// 	validatorSkipRate: Number[],
+			// 	validator_voting_performance: Number[],
+
+			// }
+
+			let foundValidators = await ValidatorsDAO.searchValidators(
+				network,
+				count,
+				names,
+				dataCenters,
+				dataCenterConcentrationScore,
+				validatorAsn,
+				softwareVersion,
+				validatorActiveStake,
+				receivedStakeFromStakePools,
+				currentValidatorCommission,
+				validatorApy,
+				validatorSkipRate,
+				validatorVotingPerformance
+			);
+
+			res.send(foundValidators);
 		} catch (err) {
 			console.error(
 				`Unable to search in Validators data in SearchController: ${err}`
