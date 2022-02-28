80f4c817080cc460151ef2ce64d6864c96b97c72
Mahdi Mostafavi <mmostafavi247@gamil.com>
2022-02-28 20:29:21 +0200

    Add new functions to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import app from "./server";
-const PORT = 8000;
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import ValidatorsDAO from "./dao/validatorsDAO";
+import CommissionHistoryDAO from "./dao/commissionHistoryDAO";
+import VotePerformanceHistoryDao from "./dao/votePerformanceHistoryDAO";
+import updateValidatorsData from "./tasks/updateValidatorsData";
 
-app.listen(PORT, () => {
-	console.log(`listening on port ${PORT}`);
-});
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+MongoClient.connect(process.env.MONGO_CONNECT_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+	.catch((err) => {
+		console.log(err);
+		process.exit(1);
+	})
+	.then(async (client) => {
+		await ValidatorsDAO.injectDB(client);
+		await CommissionHistoryDAO.injectDB(client);
+		await VotePerformanceHistoryDao.injectDB(client);
+
+		await updateValidatorsData();
+
+		app.listen(PORT, () => {
+			console.log(`listening on port ${PORT}`);
+		});
+	});
