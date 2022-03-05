64c5809aa652fff4dfeb9caf7dbba6cab9572854
Mahdi Mostafavi <mmostafavi247@gamil.com>
2022-03-05 16:07:58 +0200

    Add cors headers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import compression from "compression";
 import { searchRoutes, dataRoutes } from "./api/routes/index";
+import { cors } from "cors";
 
 const app = express();
 
+const corsOptions = {
+	origin: "*",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
